4d7f3ba00dbba651e71e6605bf38d600dd8296db
Nazar Kovalenko <77204938+Jaxer19@users.noreply.github.com>
2021-01-17 12:43:33 +0200

    Translating to Russian language

--- yesno.py
+++ yesno.py
@@ -9,7 +9,6 @@ class YesNoMod(loader.Module):
 
     @loader.unrestricted
     async def yncmd(self, message):
-        """Делает выбор жизни"""
-        list = ["Yes", "No"]
+        list = ["Да", "Нет"]
         decision = random.choice(list)
         await message.edit(str(decision))
